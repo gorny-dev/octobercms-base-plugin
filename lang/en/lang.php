26f792299ea422b54898257c97c6de2e252b0483
michal7515 <michal.gorny75@gmail.com>
2020-07-07 15:39:51 +0200

    Create fields

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,6 +4,21 @@ return [
     'plugin' => [
         'name' => 'Base',
         'description' => 'Base plugin for websites created by codeclutch',
-        'author' => 'codeclutch'
+        'author' => 'codeclutch',
+        'model' => [
+            'information' => 'Page information',
+            'title' => 'Page title',
+            'logo' => 'Logo',
+            'icon' => 'Icon',
+            'meta' => 'Meta tags',
+            'description' => 'Page description',
+            'keywords' => 'Keywords',
+            'color' => 'Main color',
+            'copyright' => 'Copyright',
+            'link' => 'Link',
+            'author_logo' => 'Author logo',
+            'author' => 'Author',
+            'keywords_placeholder' => 'write, after, commas',
+        ]
     ]
 ];
--- models/Settings.php
+++ models/Settings.php
@@ -7,8 +7,14 @@ class Settings extends Model
     public $implement = ['System.Behaviors.SettingsModel'];
 
     // A unique code
-    public $settingsCode = 'acme_demo_settings';
+    public $settingsCode = 'codeclutch_base_settings';
 
     // Reference to field configuration
     public $settingsFields = 'fields.yaml';
+
+    public $attachOne = [
+         'logo' => 'System\Models\File',
+         'icon' => 'System\Models\File',
+         'author_logo' => 'System\Models\File',
+    ];
 }
